64a4f40353123e86c6f62d72aa6608adaf3f32ee
Artem Zhmaka <zhmaka99@gmail.com>
2023-07-18 12:09:56 +0200

    fix/update_proposer_type
    Update Block proposer type

--- tests/types/block_test.go
+++ tests/types/block_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/make-software/casper-go-sdk/casper"
 	"github.com/make-software/casper-go-sdk/types"
 )
 
@@ -30,6 +31,22 @@ func Test_BlockSwitch_MarshalUnmarshal_ShouldReturnSameResult(t *testing.T) {
 	var block types.Block
 	err = json.Unmarshal(fixture, &block)
 	assert.NoError(t, err)
+
+	result, err := json.Marshal(block)
+	assert.NoError(t, err)
+	assert.JSONEq(t, string(fixture), string(result))
+}
+
+func Test_Block_Proposer_PublicKey_MarshalUnmarshal_ShouldReturnSameResult(t *testing.T) {
+	fixture, err := os.ReadFile("../data/block/block_with_system_proposer.json")
+	assert.NoError(t, err)
+
+	var block casper.ChainGetBlockResult
+	err = json.Unmarshal(fixture, &block)
+	assert.NoError(t, err)
+
+	// "00" in hex
+	assert.True(t, len(block.Block.Body.Proposer) == 1)
 
 	result, err := json.Marshal(block)
 	assert.NoError(t, err)
