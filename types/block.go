4854166d87e849e6292d2f18fa2c9fd822cd198a
Artem Zhmaka <zhmaka99@gmail.com>
2024-06-26 12:01:21 +0300

    update/actualize_chain_get_block_rc2
    Make Block structure flatten

--- types/block.go
+++ types/block.go
@@ -3,6 +3,7 @@ package types
 import (
 	"encoding/json"
 	"errors"
+
 	"github.com/make-software/casper-go-sdk/types/clvalue"
 	"github.com/make-software/casper-go-sdk/types/key"
 	"github.com/make-software/casper-go-sdk/types/keypair"
@@ -11,11 +12,34 @@ import (
 // Block represents a common object returned as result from RPC response unifying BlockV2 and BlockV1
 // Block is inherited from BlockV2, BlockV1 should be matched to the view of BlockV2 to achieve backward compatibility
 type Block struct {
-	BlockV2
-	// source BlockV1, nil if constructed from BlockV2
-	OriginBlockV1 *BlockV1
+	Hash               key.Hash                        `json:"hash"`
+	Height             uint64                          `json:"height"`
+	StateRootHash      key.Hash                        `json:"state_root_hash"`
+	ParentHash         key.Hash                        `json:"parent_hash"`
+	EraID              uint32                          `json:"era_id"`
+	Timestamp          Timestamp                       `json:"timestamp"`
+	AccumulatedSeed    *key.Hash                       `json:"accumulated_seed,omitempty"`
+	RandomBit          bool                            `json:"random_bit"`
+	CurrentGasPrice    uint8                           `json:"current_gas_price"`
+	Proposer           Proposer                        `json:"proposer"`
+	ProtocolVersion    string                          `json:"protocol_version,omitempty"`
+	EraEnd             *EraEnd                         `json:"era_end"`
+	Transactions       BlockTransactions               `json:"transactions"`
+	RewardedSignatures []SingleBlockRewardedSignatures `json:"rewarded_signatures"`
+	Proofs             []Proof                         `json:"proofs"`
 
-	Proofs []Proof `json:"proofs"`
+	// source BlockV1, nil if constructed from BlockV2
+	originBlockV1 *BlockV1
+	// source BlockV2, nil if constructed from BlockV1
+	originBlockV2 *BlockV2
+}
+
+func (b Block) GetBlockV1() *BlockV1 {
+	return b.originBlockV1
+}
+
+func (b Block) GetBlockV2() *BlockV2 {
+	return b.originBlockV2
 }
 
 // NewBlockFromBlockWithSignatures construct Block from BlockWithSignatures
@@ -26,72 +50,61 @@ func NewBlockFromBlockWithSignatures(signBlock BlockWithSignatures) Block {
 		return block
 	} else {
 		return Block{
-			BlockV2: *signBlock.Block.BlockV2,
-			Proofs:  signBlock.Proofs,
+			Hash:               signBlock.Block.BlockV2.Hash,
+			Height:             signBlock.Block.BlockV2.Header.Height,
+			StateRootHash:      signBlock.Block.BlockV2.Header.StateRootHash,
+			ParentHash:         signBlock.Block.BlockV2.Header.ParentHash,
+			EraID:              signBlock.Block.BlockV2.Header.EraID,
+			Timestamp:          signBlock.Block.BlockV2.Header.Timestamp,
+			AccumulatedSeed:    signBlock.Block.BlockV2.Header.AccumulatedSeed,
+			RandomBit:          signBlock.Block.BlockV2.Header.RandomBit,
+			CurrentGasPrice:    signBlock.Block.BlockV2.Header.CurrentGasPrice,
+			Proposer:           signBlock.Block.BlockV2.Header.Proposer,
+			ProtocolVersion:    signBlock.Block.BlockV2.Header.ProtocolVersion,
+			EraEnd:             NewEraEndFromV2(signBlock.Block.BlockV2.Header.EraEnd),
+			Transactions:       signBlock.Block.BlockV2.Body.Transactions,
+			RewardedSignatures: signBlock.Block.BlockV2.Body.RewardedSignatures,
+			Proofs:             signBlock.Proofs,
+			originBlockV2:      signBlock.Block.BlockV2,
 		}
 	}
 }
 
 // NewBlockFromBlockV1 construct Block from BlockV1
-func NewBlockFromBlockV1(block BlockV1) Block {
-	var eraEnd EraEndV2
-	if block.Header.EraEnd != nil {
-		rewards := make(map[string][]clvalue.UInt512, len(block.Header.EraEnd.EraReport.Rewards))
-		for _, reward := range block.Header.EraEnd.EraReport.Rewards {
-			list := rewards[reward.Validator.ToHex()]
-			list = append(list, reward.Amount)
-			rewards[reward.Validator.ToHex()] = list
-		}
-
-		eraEnd = EraEndV2{
-			NextEraGasPrice:         1,
-			Equivocators:            block.Header.EraEnd.EraReport.Equivocators,
-			InactiveValidators:      block.Header.EraEnd.EraReport.InactiveValidators,
-			NextEraValidatorWeights: block.Header.EraEnd.NextEraValidatorWeights,
-			Rewards:                 rewards,
-		}
-	}
-
+func NewBlockFromBlockV1(blockV1 BlockV1) Block {
 	blockTransactions := make(BlockTransactions, 0)
-	for i := range block.Body.TransferHashes {
+	for i := range blockV1.Body.TransferHashes {
 		blockTransactions = append(blockTransactions, BlockTransaction{
 			Category: BlockTransactionCategoryMint,
 			Version:  BlockTransactionDeploy,
-			Hash:     block.Body.TransferHashes[i],
+			Hash:     blockV1.Body.TransferHashes[i],
 		})
 	}
 
-	for i := range block.Body.DeployHashes {
+	for i := range blockV1.Body.DeployHashes {
 		blockTransactions = append(blockTransactions, BlockTransaction{
 			Category: BlockTransactionCategoryLarge,
 			Version:  BlockTransactionDeploy,
-			Hash:     block.Body.DeployHashes[i],
+			Hash:     blockV1.Body.DeployHashes[i],
 		})
 	}
 
 	return Block{
-		BlockV2: BlockV2{
-			Hash: block.Hash,
-			Header: BlockHeaderV2{
-				AccumulatedSeed: block.Header.AccumulatedSeed,
-				BodyHash:        block.Header.BodyHash,
-				EraID:           block.Header.EraID,
-				CurrentGasPrice: 1,
-				Height:          block.Header.Height,
-				ParentHash:      block.Header.ParentHash,
-				ProtocolVersion: block.Header.ProtocolVersion,
-				RandomBit:       block.Header.RandomBit,
-				StateRootHash:   block.Header.StateRootHash,
-				Timestamp:       block.Header.Timestamp,
-				EraEnd:          &eraEnd,
-				Proposer:        block.Body.Proposer,
-			},
-			Body: BlockBodyV2{
-				Transactions: blockTransactions,
-			},
-		},
-		OriginBlockV1: &block,
-		Proofs:        block.Proofs,
+		Hash:            blockV1.Hash,
+		Height:          blockV1.Header.Height,
+		StateRootHash:   blockV1.Header.StateRootHash,
+		ParentHash:      blockV1.Header.ParentHash,
+		EraID:           blockV1.Header.EraID,
+		Timestamp:       blockV1.Header.Timestamp,
+		AccumulatedSeed: blockV1.Header.AccumulatedSeed,
+		RandomBit:       false,
+		CurrentGasPrice: 1,
+		Proposer:        blockV1.Body.Proposer,
+		ProtocolVersion: blockV1.Header.ProtocolVersion,
+		EraEnd:          NewEraEndFromV1(blockV1.Header.EraEnd),
+		Transactions:    blockTransactions,
+		Proofs:          blockV1.Proofs,
+		originBlockV1:   &blockV1,
 	}
 }
 
@@ -127,14 +140,34 @@ type BlockHeaderWrapper struct {
 }
 
 type BlockHeader struct {
-	BlockHeaderV2
+	AccumulatedSeed *key.Hash `json:"accumulated_seed,omitempty"`
+	BodyHash        key.Hash  `json:"body_hash"`
+	EraID           uint32    `json:"era_id"`
+	CurrentGasPrice uint8     `json:"current_gas_price"`
+	Height          uint64    `json:"height"`
+	ParentHash      key.Hash  `json:"parent_hash"`
+	Proposer        Proposer  `json:"proposer"`
+	ProtocolVersion string    `json:"protocol_version,omitempty"`
+	RandomBit       bool      `json:"random_bit"`
+	StateRootHash   key.Hash  `json:"state_root_hash"`
+	Timestamp       Timestamp `json:"timestamp"`
+	EraEnd          *EraEnd   `json:"era_end"`
 
 	// source OriginBlockHeaderV1, nil if constructed from BlockHeaderV2
-	OriginBlockHeaderV1 *BlockHeaderV1
+	originBlockHeaderV1 *BlockHeaderV1
+	// source OriginBlockHeaderV2, nil if constructed from BlockHeaderV1
+	originBlockHeaderV2 *BlockHeaderV2
+}
+
+func (b BlockHeader) GetBlockHeaderV1() *BlockHeaderV1 {
+	return b.originBlockHeaderV1
+}
+
+func (b BlockHeader) GetBlockHeaderV2() *BlockHeaderV2 {
+	return b.originBlockHeaderV2
 }
 
 func NewBlockHeaderFromV1(header BlockHeaderV1) BlockHeader {
-	var eraEnd *EraEndV2
 	if header.EraEnd != nil {
 		rewards := make(map[string][]clvalue.UInt512, len(header.EraEnd.EraReport.Rewards))
 		for _, reward := range header.EraEnd.EraReport.Rewards {
@@ -142,33 +175,40 @@ func NewBlockHeaderFromV1(header BlockHeaderV1) BlockHeader {
 			list = append(list, reward.Amount)
 			rewards[reward.Validator.ToHex()] = list
 		}
-
-		eraEnd = &EraEndV2{
-			Equivocators:            header.EraEnd.EraReport.Equivocators,
-			InactiveValidators:      header.EraEnd.EraReport.InactiveValidators,
-			NextEraValidatorWeights: header.EraEnd.NextEraValidatorWeights,
-			Rewards:                 rewards,
-			NextEraGasPrice:         1,
-		}
 	}
 
 	return BlockHeader{
-		BlockHeaderV2: BlockHeaderV2{
-			AccumulatedSeed: header.AccumulatedSeed,
-			BodyHash:        header.BodyHash,
-			EraID:           header.EraID,
-			CurrentGasPrice: 1,
-			Height:          header.Height,
-			ParentHash:      header.ParentHash,
-			ProtocolVersion: header.ProtocolVersion,
-			RandomBit:       header.RandomBit,
-			StateRootHash:   header.StateRootHash,
-			Timestamp:       header.Timestamp,
-			EraEnd:          eraEnd,
-		},
-		OriginBlockHeaderV1: &header,
+		AccumulatedSeed:     header.AccumulatedSeed,
+		BodyHash:            header.BodyHash,
+		EraID:               header.EraID,
+		CurrentGasPrice:     1,
+		Height:              header.Height,
+		ParentHash:          header.ParentHash,
+		ProtocolVersion:     header.ProtocolVersion,
+		RandomBit:           header.RandomBit,
+		StateRootHash:       header.StateRootHash,
+		Timestamp:           header.Timestamp,
+		EraEnd:              NewEraEndFromV1(header.EraEnd),
+		originBlockHeaderV1: &header,
 	}
+}
 
+func NewBlockHeaderFromV2(header BlockHeaderV2) BlockHeader {
+	return BlockHeader{
+		AccumulatedSeed:     header.AccumulatedSeed,
+		BodyHash:            header.BodyHash,
+		EraID:               header.EraID,
+		CurrentGasPrice:     header.CurrentGasPrice,
+		Height:              header.Height,
+		ParentHash:          header.ParentHash,
+		Proposer:            header.Proposer,
+		ProtocolVersion:     header.ProtocolVersion,
+		RandomBit:           header.RandomBit,
+		StateRootHash:       header.StateRootHash,
+		Timestamp:           header.Timestamp,
+		EraEnd:              NewEraEndFromV2(header.EraEnd),
+		originBlockHeaderV2: &header,
+	}
 }
 
 type BlockHeaderV1 struct {
@@ -251,30 +291,6 @@ type BlockTransaction struct {
 	Hash     key.Hash
 }
 
-func getBlockTransactionsFromTransactionHashes(hashes []TransactionHash, category BlockTransactionCategory) BlockTransactions {
-	if len(hashes) == 0 {
-		return nil
-	}
-
-	res := make(BlockTransactions, 0)
-	for i := range hashes {
-		blockTransaction := BlockTransaction{
-			Category: category,
-		}
-		if hashes[i].TransactionV1Hash != nil {
-			blockTransaction.Hash = *hashes[i].TransactionV1Hash
-			blockTransaction.Version = BlockTransactionVersionV1
-		} else {
-			blockTransaction.Hash = *hashes[i].Deploy
-			blockTransaction.Version = BlockTransactionDeploy
-		}
-
-		res = append(res, blockTransaction)
-	}
-
-	return res
-}
-
 type BlockBodyV2 struct {
 	// Map of transactions mapping categories to a list of transaction hashes.
 	Transactions BlockTransactions `json:"transactions"`
@@ -303,4 +319,28 @@ type Proof struct {
 	PublicKey keypair.PublicKey `json:"public_key"`
 	// Validator signature
 	Signature HexBytes `json:"signature"`
+}
+
+func getBlockTransactionsFromTransactionHashes(hashes []TransactionHash, category BlockTransactionCategory) BlockTransactions {
+	if len(hashes) == 0 {
+		return nil
+	}
+
+	res := make(BlockTransactions, 0)
+	for i := range hashes {
+		blockTransaction := BlockTransaction{
+			Category: category,
+		}
+		if hashes[i].TransactionV1Hash != nil {
+			blockTransaction.Hash = *hashes[i].TransactionV1Hash
+			blockTransaction.Version = BlockTransactionVersionV1
+		} else {
+			blockTransaction.Hash = *hashes[i].Deploy
+			blockTransaction.Version = BlockTransactionDeploy
+		}
+
+		res = append(res, blockTransaction)
+	}
+
+	return res
 }
