42969218d1bc4f24c3421ab42d56c62e323a53d4
ZhmakAS <zhmaka99@gmail.com>
2024-12-19 20:18:06 +0100

    Update transaction target serialization (#126)
    Update transaction target serialization

--- types/transaction_runtime.go
+++ types/transaction_runtime.go
@@ -1,6 +1,11 @@
 package types
 
-// TransactionRuntime SmartContract transaction types.
+import (
+	"github.com/make-software/casper-go-sdk/v2/types/serialization"
+	"github.com/make-software/casper-go-sdk/v2/types/serialization/encoding"
+)
+
+// TransactionRuntime Package transaction types.
 type TransactionRuntime string
 
 const (
@@ -20,4 +25,21 @@ func (t TransactionRuntime) RuntimeTag() byte {
 		return TransactionRuntimeTagVmCasperV2
 	}
 	return 0
+}
+
+func (t *TransactionRuntime) Bytes() ([]byte, error) {
+	builder, err := serialization.NewCallTableSerializationEnvelopeBuilder(t.serializedFieldLengths())
+	if err != nil {
+		return nil, err
+	}
+	if err = builder.AddField(TagFieldIndex, []byte{t.RuntimeTag()}); err != nil {
+		return nil, err
+	}
+	return builder.BinaryPayloadBytes()
+}
+
+func (t TransactionRuntime) serializedFieldLengths() []int {
+	return []int{
+		encoding.U8SerializedLength,
+	}
 }
