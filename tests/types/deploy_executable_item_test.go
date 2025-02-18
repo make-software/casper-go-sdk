bf9f5d267ee0dae0b9dd7365b0b762046813a8c8
Volodymyr-Kuchinskyi <kuchinskiy.wladimir@gmail.com>
2025-02-18 14:36:10 -0300

    fix: Fixed stored version by hash and stored version by name serialization

--- tests/types/deploy_executable_item_test.go
+++ tests/types/deploy_executable_item_test.go
@@ -17,33 +17,44 @@ func Test_ExecutableItem_MarshalUnmarshal_ShouldBeSameResult(t *testing.T) {
 	}{
 		{
 			"item with module bytes",
-			`{"ModuleBytes": {"module_bytes": "[72 bytes]","args": [["testName", "testVal"]]}}`,
+			`{"ModuleBytes": {"module_bytes": "c4c411864f7b717c27839e56f6f1ebe5da3f35ec0043f437324325d65a22afa4","args": [["amount",{"bytes":"060040f538a406","parsed":"7302400000000","cl_type":"U512"}],["target",{"bytes":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","parsed":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","cl_type":"PublicKey"}],["id",{"bytes":"016dec181a95010000","parsed":1739899595885,"cl_type":{"Option":"U64"}}]]}}`,
 		},
 		{
 			"item with stored contract by hash",
-			`{"StoredContractByHash": {"hash": "c4c411864f7b717c27839e56f6f1ebe5da3f35ec0043f437324325d65a22afa4","entry_point": "pclphXwfYmCmdITj8hnh","args": [["testName", "testVal"]]}}`,
+			`{"StoredContractByHash": {"hash": "c4c411864f7b717c27839e56f6f1ebe5da3f35ec0043f437324325d65a22afa4","entry_point": "pclphXwfYmCmdITj8hnh","args": [["amount",{"bytes":"060040f538a406","parsed":"7302400000000","cl_type":"U512"}],["target",{"bytes":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","parsed":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","cl_type":"PublicKey"}],["id",{"bytes":"016dec181a95010000","parsed":1739899595885,"cl_type":{"Option":"U64"}}]]}}`,
 		},
 		{
 			"item with stored contract by name",
-			`{"StoredContractByName": {"name": "U5A74bSZH8abT8HqVaK9","entry_point": "gIetSxltnRDvMhWdxTqQ","args": [["testName", "testVal"]]}}`,
+			`{"StoredContractByName": {"name": "U5A74bSZH8abT8HqVaK9","entry_point": "gIetSxltnRDvMhWdxTqQ","args": [["amount",{"bytes":"060040f538a406","parsed":"7302400000000","cl_type":"U512"}],["target",{"bytes":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","parsed":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","cl_type":"PublicKey"}],["id",{"bytes":"016dec181a95010000","parsed":1739899595885,"cl_type":{"Option":"U64"}}]]}}`,
 		},
 		{
-			"item with stored versioned contract by hash",
-			`{"StoredContractByHash": {"hash": "c4c411864f7b717c27839e56f6f1ebe5da3f35ec0043f437324325d65a22afa4","entry_point": "pclphXwfYmCmdITj8hnh","args": [["testName", "testVal"]]}}`,
+			"item with stored versioned contract by hash without version",
+			`{"StoredVersionedContractByHash": {"hash": "c4c411864f7b717c27839e56f6f1ebe5da3f35ec0043f437324325d65a22afa4","entry_point": "pclphXwfYmCmdITj8hnh","version": null,"args": [["amount",{"bytes":"060040f538a406","parsed":"7302400000000","cl_type":"U512"}],["target",{"bytes":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","parsed":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","cl_type":"PublicKey"}],["id",{"bytes":"016dec181a95010000","parsed":1739899595885,"cl_type":{"Option":"U64"}}]]}}`,
 		},
 		{
-			"item with stored versioned contract by name",
-			`{"StoredVersionedContractByName": {"name": "lWJWKdZUEudSakJzw1tn","version": 1632552656, "entry_point": "S1cXRT3E1jyFlWBAIVQ8","args": [["testName", "testVal"]]}}`,
+			"item with stored versioned contract by hash with version",
+			`{"StoredVersionedContractByHash": {"hash": "c4c411864f7b717c27839e56f6f1ebe5da3f35ec0043f437324325d65a22afa4","entry_point": "pclphXwfYmCmdITj8hnh","version": 1,"args": [["amount",{"bytes":"060040f538a406","parsed":"7302400000000","cl_type":"U512"}],["target",{"bytes":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","parsed":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","cl_type":"PublicKey"}],["id",{"bytes":"016dec181a95010000","parsed":1739899595885,"cl_type":{"Option":"U64"}}]]}}`,
+		},
+		{
+			"item with stored versioned contract by name with version",
+			`{"StoredVersionedContractByName": {"name": "lWJWKdZUEudSakJzw1tn","version": 1632552656, "entry_point": "S1cXRT3E1jyFlWBAIVQ8","args": [["amount",{"bytes":"060040f538a406","parsed":"7302400000000","cl_type":"U512"}],["target",{"bytes":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","parsed":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","cl_type":"PublicKey"}],["id",{"bytes":"016dec181a95010000","parsed":1739899595885,"cl_type":{"Option":"U64"}}]]}}`,
+		},
+		{
+			"item with stored versioned contract by name without version",
+			`{"StoredVersionedContractByName": {"name": "lWJWKdZUEudSakJzw1tn","version": null, "entry_point": "S1cXRT3E1jyFlWBAIVQ8","args": [["amount",{"bytes":"060040f538a406","parsed":"7302400000000","cl_type":"U512"}],["target",{"bytes":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","parsed":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","cl_type":"PublicKey"}],["id",{"bytes":"016dec181a95010000","parsed":1739899595885,"cl_type":{"Option":"U64"}}]]}}`,
 		},
 		{
 			"item with stored transfer",
-			`{"Transfer": {"args": [["testName", "testVal"]]}}`,
+			`{"Transfer": {"args": [["amount",{"bytes":"060040f538a406","parsed":"7302400000000","cl_type":"U512"}],["target",{"bytes":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","parsed":"011c74ebfcc1b19bc3e578bec3ecfa2d484f2a00d7e9e8152c4c70f519f6a89f6a","cl_type":"PublicKey"}],["id",{"bytes":"016dec181a95010000","parsed":1739899595885,"cl_type":{"Option":"U64"}}]]}}`,
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			var deployItem types.ExecutableDeployItem
 			err := json.Unmarshal([]byte(test.data), &deployItem)
+			require.NoError(t, err)
+
+			_, err = deployItem.Bytes()
 			require.NoError(t, err)
 
 			result, err := json.Marshal(deployItem)
